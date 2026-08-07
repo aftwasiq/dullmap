9dfed26fe28f7424ac23f1995502c4380014cab3
aftwasiq <wasiq.abdullah.21@gmail.com>
2026-08-07 01:24:08 -0400

    added example.map

--- dullmap.c
+++ dullmap.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
+/* 
+ * some notes
+ *
+ * start off by parsing given .map file
+ * split memory addresses into blocks based on size
+ *
+ * for example
+ * [ 0x08000000 ] -> ISR_VECTOR
+ *
+ */
+
 int main(int argc, char *argv) {
   
   return 0;
