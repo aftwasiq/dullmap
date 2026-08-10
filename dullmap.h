1b455a2e16fce2ffcf847150448e24d21ecbae57
aftwasiq <wasiq.abdullah.21@gmail.com>
2026-08-09 20:01:38 -0400

    derp

--- dullmap.h
+++ dullmap.h
@@ -13,14 +13,12 @@ extern "C" {
  * @brief Creates new dullmap window
  *
  * @param height signifies the window height
- *
  * @param width signifies the window width
- *
  * @param x is the x coordinate we begin with
- *
  * @param y is the y coordinate we begin with
  *
  * @retval dullmap as an ncurses WINDOW struct
+ *
  */
 
 WINDOW *create_dullmap(int height, int width, int y, int x);
@@ -31,13 +29,11 @@ WINDOW *create_dullmap(int height, int width, int y, int x);
  * @param window is the dullmap WINDOW struct to destroy
  *
  * @retval 0 on failure
- *
  * @retval 1 on success
+ *
  */
 
 int destroy_dullmap(WINDOW *window);
-
-
 
 
 #ifdef __cplusplus
